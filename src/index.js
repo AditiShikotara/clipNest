334fffa57ca0e41a8d8986107d5d3275726a200f
Aditi <ams494shikotara@gmail.com>
2026-08-03 12:43:32 +0530

    MongoDB Connection done

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import dns from "dns";
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: "./.env",
+});
+
+
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
+
+connectDB()
